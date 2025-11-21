5cff34ad679b6d2d3a9533da6289ebcce653d269
Ivo Tonev <ivo@tonev.pro.br>
2025-11-21 18:53:37 -0300

    Add files via upload

---
+++ fgt_remover_regras.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "#!/bin/sh" 
+echo "# \\"
+echo "exec /usr/bin/expect -- \"\$0\" \${1+\"\$@\"}"
+echo ""
+echo "exp_version -exit 5.0"
+echo ""
+echo "set FW_HOST [lindex \$argv 0]"
+echo "set FW_PORT [lindex \$argv 1]"
+echo "set FW_USERNAME [lindex \$argv 2]"
+echo "set FW_PASSWORD [lindex \$argv 3]"
+echo ""
+echo "spawn ssh -p \$FW_PORT \$FW_USERNAME@\$FW_HOST"
+echo ""
+echo "expect \"password:\""
+echo 'send "$FW_PASSWORD\\r"'
+echo "expect \"#\""
+echo ""
+
+RULESFILE=$1
+while IFS= read -r line
+do
+	echo -n "send \""
+	echo -n "$line"
+	echo -n "\\"
+	echo -n "r"
+	echo "\""
+	echo "sleep 1"
+	echo "expect \"#\""
+	echo "sleep 1"
+done < "$RULESFILE"
+
+echo -n "send \"quit "
+echo -n "\\"
+echo "r\""
